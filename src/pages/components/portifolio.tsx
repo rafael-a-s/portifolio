aea9707bcd2cd17af19090d1b5bcd3663b17b87e
rafael-a-s <rafinha.rafaelaguiar@gmail.com>
2023-03-21 13:16:53 -0300

    Implementa service

---
+++ src/pages/components/portifolio.tsx
@@ -0,0 +1,9 @@
+import style from '../../styles/Portifolio.module.css';
+
+export default function Portifolio() {
+  return (
+    <section className={style.portifolio} id={style.portifolio}>
+      
+    </section>
+  )
+}
---
+++ src/pages/components/services.tsx
@@ -0,0 +1,33 @@
+import { FiCode } from 'react-icons/fi';
+import style from '../../styles/Services.module.css';
+import Button from '../utils/button';
+
+export default function Services() {
+  return (
+    <section className={style.services} id={style.services}>
+      <h2 className={style.heading}>Nossos <span>Serviços</span></h2>
+      <div className={style.services_container}>
+        <div className={style.services_box}>
+          <FiCode className={style.icons}/>
+          <h3>Web Development</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum minima nobis amet deserunt, possimus debitis tempora reiciendis magni maiores nihil perferendis id.</p>
+          <Button text='Leia mais'/>
+        </div>
+
+        <div className={style.services_box}>
+        <FiCode className={style.icons}/>
+          <h3>Back-end Development</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum minima nobis amet deserunt, possimus debitis tempora reiciendis magni maiores nihil perferendis id.</p>
+          <Button text='Leia mais'/>
+        </div>
+
+        <div className={style.services_box}>
+          <FiCode className={style.icons}/>
+          <h3>Azure DevOps</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum minima nobis amet deserunt, possimus debitis tempora reiciendis magni maiores nihil perferendis id.</p>
+          <Button text='Leia mais'/>
+        </div>
+      </div>
+    </section>
+  )
+}
