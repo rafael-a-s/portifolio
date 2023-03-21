702894ab64feabc30d4df74aa547be86a631475d
rafael-a-s <rafinha.rafaelaguiar@gmail.com>
2023-03-21 10:30:50 -0300

    Implmenta Home

---
+++ src/pages/components/home.tsx
@@ -0,0 +1,26 @@
+import { FiFacebook, FiTwitter, FiInstagram, FiLinkedin } from 'react-icons/fi';
+import style from '../../styles/Home.module.css';
+import Button from '../utils/button';
+import avatar from '/public/images/avatar.png';
+export default function Home(){
+  return (
+    <section className={style.home} id={style.home}>
+      <div className={style.home_content}>
+        <h3>Olá, Me chamo</h3>
+        <h1>Rafael Aguiar</h1>
+        <h3>E sou desenvolvedor <span>FullStack</span></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, error nobis facere quo odio, esse illum molestias sed, id explicabo at labore quae minima numquam non harum possimus necessitatibus! Officiis!</p>
+        <div className={style.social_media}>
+          <a href="#"> <FiFacebook/> </a>
+          <a href="#"> <FiTwitter/> </a>
+          <a href="#"> <FiInstagram/> </a>
+          <a href="#"> <FiLinkedin/> </a>
+        </div>
+        <Button text='Download CV'/>
+      </div>
+      <div className={style.home_img}>
+        <img src={avatar.src} alt="Avatar de um desenvolvedor" />
+      </div>
+    </section>
+  )
+}
