ed253412a239c9b253204fe83dad2a8997b45f8f
rafael-a-s <rafinha.rafaelaguiar@gmail.com>
2023-03-26 19:30:38 -0300

    feat: Implement Typed.js

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,15 @@ import Button from '../public/components/button/button';
 import avatar from '../public/images/avatar.png';
 import React, { useEffect } from 'react';
 import { configScrollReveal } from '../public/config/configScrollReveal';
+import Typed from 'typed.js';
+import { configTyped } from '../public/config/configTyped';
 
 export default function Page() {
   const refToComponent = React.useRef(null);
   const refToImg = React.useRef(null);
   const refToHeder = React.useRef(null);
   const refToParagraf = React.useRef(null);
+  const refToWrite = React.useRef(null);
 
   useEffect(() => {
     async function animate() {
@@ -22,15 +25,20 @@ export default function Page() {
       refToParagraf.current ?  sr().reveal(refToParagraf.current, {origin: 'right'} ) : null;
     }
     animate()
+    
+    const typed = new Typed(refToWrite.current, configTyped);
+
+    return () => {
+      typed.destroy();
+    };
   },[]);
 
- 
   return (
     <section className={style.home} id={style.home}>
       <div className={style.home_content} ref={refToComponent}>
         <h3>Olá, Me chamo</h3>
         <h1 ref={refToHeder}>Rafael Aguiar</h1>
-        <h3>E sou desenvolvedor <span>FullStack</span></h3>
+        <h3>E sou desenvolvedor <span ref={refToWrite}></span></h3>
         <p ref={refToParagraf}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, error nobis facere quo odio, esse illum molestias sed, id explicabo at labore quae minima numquam non harum possimus necessitatibus! Officiis!</p>
         <div className={style.social_media}>
           <a href="#"> <FiFacebook/> </a>
---
+++ public/config/configTyped.ts
@@ -0,0 +1,7 @@
+export const configTyped = {
+  strings: ['Frontend Developer', 'Backend Develop', 'Student'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+}
