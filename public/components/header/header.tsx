fca4030c64731a8d80c995e0ed341d416c587149
rafael-a-s <rafinha.rafaelaguiar@gmail.com>
2023-03-26 17:36:33 -0300

    feat: Responsive

--- public/components/header/header.tsx
+++ public/components/header/header.tsx
@@ -1,16 +1,18 @@
 'use client';
 import style from '../../../styles/Header.module.css';
-import { BiMenu } from 'react-icons/bi';
+import { BiMenu, BiArrowToRight } from 'react-icons/bi';
 import { useSelectedLayoutSegment } from 'next/navigation';
+import { useState } from 'react';
 
 export default function HeaderPage(){
   const segments = useSelectedLayoutSegment();
+  const [menu, setMenu] = useState(true);
 
   return (
     <header className={style.header}>
       <a href="/" className={style.logo}>Portifolio.</a>
-      <BiMenu id={style.menu_icon}/>
-      <nav className={style.navbar}>
+      { menu ? <BiMenu onClick={ () => setMenu(!menu) } id={style.menu_icon}/> : <BiArrowToRight onClick={ () => setMenu(!menu) } id={style.menu_icon}/> }
+      <nav className={`${style.navbar} ${menu ? '' : style.active}`}>
         <a href="/" className={ segments === null ? style.active : ''}>Home</a>
         <a href="/services" className={ segments === 'services' ? style.active : ''} >Experiêcias</a>
         <a href="/portifolio" className={ segments === 'portifolio' ? style.active : ''}>Projetos</a>
